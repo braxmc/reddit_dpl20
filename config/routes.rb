c983aedaab34c41a1edfa6073349966f31df859f
Braxton McClellan <thebraxmc@gmail.com>
2020-09-02 18:50:45 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
 Rails.application.routes.draw do
- # resources
- # get '/newform', to: 'ppl#new'
- root 'subs#index'
+  # resources
+  # get '/newform', to: 'ppl#new'
+  root 'subs#index'
+  # get '/subs/new', to: 'subs#new'
+  # get '/subs', to: 'subs#index'
+  #             3
+  # get '/subs/:id', to: 'subs#show'
+  # post '/subs', to: 'subs#create'
 
- # get '/subs/new', to: 'sub#new'
- # get '/subs', to: 'subs#index'
- #              3
- # get '/subs/:id', to: 'subs#show'
- # post '/subs', to: 'subs#create'
- resources :subs
+  # dont do this
+  # resources :subs do
+  #   resources :topics
+  #     resources :comments
+  # end
+
+  resources :subs do
+    resources :topics
+  end
+
+  resources :topics do
+    resources :comments
+  end
+
+  # custom routes
+  # http verb '/url you want it to go', :to 'controller#action'
+
 end
